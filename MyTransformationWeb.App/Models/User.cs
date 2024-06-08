21fcc779b176943be6551e7007ae33c09b45fba0
Carlos Herrera <juan.cervantes@rappi.com>
2024-06-08 16:03:25 -0600

    MT-8 Create the users section

---
+++ MyTransformationWeb.App/Models/User.cs
@@ -0,0 +1,30 @@
+using Newtonsoft.Json;
+
+namespace MyTransformationWeb.App.Models;
+
+public class User
+{
+    [JsonProperty(nameof(Id))]
+    public string Id { get; set; }
+
+    [JsonProperty(nameof(Email))]
+    public string Email { get; set; }
+
+    [JsonProperty(nameof(FirstName))]
+    public string FirstName { get; set; }
+
+    [JsonProperty(nameof(LastName))]
+    public string LastName { get; set; }
+
+    [JsonProperty(nameof(Birthdate))]
+    public DateTime? Birthdate { get; set; }
+
+    [JsonProperty(nameof(ProfilePicture))]
+    public string ProfilePicture { get; set; }
+
+    [JsonProperty(nameof(CreatedAt))]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [JsonProperty(nameof(UpdatedAt))]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
