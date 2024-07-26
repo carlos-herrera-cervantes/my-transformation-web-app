1cb03c912963e80a8405e0499f8bf274e45c3b9a
Carlos Herrera <juan.cervantes@rappi.com>
2024-07-26 15:32:19 -0600

    MT-10 Create the progress section

---
+++ MyTransformationWeb.App/Models/UserEvolution.cs
@@ -0,0 +1,34 @@
+using Newtonsoft.Json;
+
+public class UserEvolution
+{
+    [JsonProperty(nameof(Id))]
+    public string Id { get; set; }
+
+    [JsonProperty(nameof(UserId))]
+    public string UserId { get; set; }
+
+    [JsonProperty(nameof(ExerciseId))]
+    public string ExerciseId { get; set; }
+
+    [JsonProperty(nameof(Weight))]
+    public int? Weight { get; set; }
+
+    [JsonProperty(nameof(MeasurementUnit))]
+    public string MeasurementUnit { get; set; }
+
+    [JsonProperty(nameof(Reps))]
+    public int? Reps { get; set; }
+
+    [JsonProperty(nameof(Comment))]
+    public string Comment { get; set; }
+
+    [JsonProperty(nameof(Moment))]
+    public DateTime? Moment { get; set; }
+
+    [JsonProperty(nameof(CreatedAt))]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [JsonProperty(nameof(UpdatedAt))]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
